e4249bea567a7f6a27982c0a2bab0339239b6518
ytsubhadip <159522165+ytsubhadip@users.noreply.github.com>
2025-09-07 22:09:08 +0530

    update

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
+import joblib
+model = joblib.load('polynomial_regression_model.joblib')
+
+def heart_predict(demo):
+    pre = model.predict([demo])
+    return pre
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def home():
-    return render_template("index.html")
+
+    if request.method == 'POST':
+        age = int(request.form.get('age'))
+        exercise= float(request.form.get('exercise'))
+        bmi = float(request.form.get('bmi'))
+        bp = float(request.form.get('bp'))
+        cholesterol = float(request.form.get('cholesterol'))
+        print(age, exercise, bmi, bp,cholesterol)
+        pre = heart_predict([age,exercise,bmi,bp,cholesterol])
+
+    return render_template("index.html", pre = pre)
+
 
 app.run(debug=True)
